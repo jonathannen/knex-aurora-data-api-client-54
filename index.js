935756152c31423c3d85423cbabc5be9fe040a42
Jon Williams <jon@jonathannen.com>
2021-04-12 11:01:21 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const knex = require('knex');
+const dataApi = require('knex-aurora-data-api-client')
+
+// const config = {
+//   client: dataApi.postgres,
+//   connection: {
+//     resourceArn: 'arn:111',
+//     secretArn: 'arn:222',
+//   },
+// };
+
+// knex(config);
